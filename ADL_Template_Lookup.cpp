e897d48b2a5282a97382a62fc1761e49a0409a61
Mohamed KHABOU <mohameding.khbou@gmail.com>
2024-04-05 06:36:09 +0100

    ADL_Template_Lookup_1

--- ADL_Template_Lookup.cpp
+++ ADL_Template_Lookup.cpp
@@ -1,29 +1,35 @@
-    #include <iostream>
+#include <iostream>
 using namespace std;
 
+// Template function adl
 template<typename T>
 void adl(T)
 {
-  cout << "T";
+  cout << "T"; // Print "T" for any type T
 }
 
+// Structure S
 struct S
 {
 };
 
+// Function adl specialization for type S
+void adl(S)
+{
+  cout << "S"; // Print "S" when called with an object of type S
+}
+
+// Function template call_adl
 template<typename T>
 void call_adl(T t)
 {
-  adl(S());
-  adl(t);
+  adl(S()); // First adl call: Non-dependent call, uses the generic adl(T) at compile time
+  adl(t);   // Second adl call: Dependent call, resolves to specialized adl(S) at instantiation
 }
 
-void adl(S)
-{
-  cout << "S";
-}
-
+// Main function
 int main ()
 {
-  call_adl(S());
+  call_adl(S()); // Call the function call_adl with an object of type S
 }
+
