445fe379701f0a6ca76aa9776d8505e56358bbd2
Mohamed KHABOU <mohameding.khbou@gmail.com>
2024-04-06 08:07:26 +0100

    modify_conana

--- conanfile.py
+++ conanfile.py
@@ -2,46 +2,13 @@ from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 
-class helloRecipe(ConanFile):
-    name = "hello"
-    version = "1.0"
+class Pkg(ConanFile):
+   name = "mypkg"
+   # see, no version defined!
+   def requirements(self):
+       # this trick allow to depend on packages on your same user/channel
+       self.requires("dep/0.3@%s/%s" % (self.user, self.channel))
 
-    # Optional metadata
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of hello package here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
-
-    # Binary configuration
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-
-    # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "src/*", "include/*"
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def layout(self):
-        cmake_layout(self)
-
-    def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
-        tc = CMakeToolchain(self)
-        tc.generate()
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-
-    def package(self):
-        cmake = CMake(self)
-        cmake.install()
-
-    def package_info(self):
-        self.cpp_info.libs = ["hello"]
+   def build(self):
+       if self.version == "myversion":
+           # something specific for this version of the package.
